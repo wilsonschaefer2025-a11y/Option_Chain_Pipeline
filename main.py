dfc2104b0645864970204d8a3c9ee468b7057f1c
wilsonschaefer2025-a11y <wilsonschaefer2025@u.northwestern.edu>
2026-09-21 19:08:26 -0500

    Comment Added to main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ from src.data.cleaning_options import (
 from src.pricing.smile import fit_volatility_smile
 from src.visualization import plot_smile, plot_price_comparison
 
+#Note: Currently built with Claude. Looking at revamping myself later but my main focus
+#of the project was the backend of the pipeline and less so the presentation. 
 
 def clean_chain(chain, S, r, T, sigma, option_type):
     chain = filter_no_arbitrage(chain, S, r, T, option_type=option_type)
